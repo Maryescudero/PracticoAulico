9860fd300d9aa23c02505366a33a3f17ddf9431b
Marilena <marusofok2@gmail.com>
2023-08-30 01:12:33 -0300

    aulico 2

---
+++ src/practicoaulico2/PracticoAulico2.java
@@ -0,0 +1,17 @@
+
+package practicoaulico2;
+
+/**
+ *
+ * @author marus
+ */
+public class PracticoAulico2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
